4720d97bad8832295c7ccf9dfc02ecfab15b4250
Ruslan Krokhin <ulitcos@gmail.com>
2023-06-17 19:52:21 +0200

    Added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2016": true
+  env: {
+    browser: true,
+    es2016: true,
   },
-  "extends": ["airbnb-base", "airbnb-typescript"],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-    "project": "./tsconfig.json"
+  extends: ["airbnb-base", "airbnb-typescript", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
-  "rules": {
+  rules: {
     "react/jsx-filename-extension": "off", // it does not react
     "@typescript-eslint/no-unused-expressions": "off",
     "import/prefer-default-export": "off",
     "import/no-dynamic-require": "off",
     "global-require": "off",
     "linebreak-style": "off",
-    "max-len": ["error", {"code": 120}],
-    "prefer-arrow-callback": ["error", {"allowNamedFunctions": true}],
+    "max-len": ["error", { code: 120 }],
+    "prefer-arrow-callback": ["error", { allowNamedFunctions: true }],
     "no-console": ["error", { allow: ["warn", "error"] }],
     "react/prop-types": "off",
     "react/jsx-props-no-spreading": "off",
     "consistent-return": "off",
   },
-  "ignorePatterns": ["*.scss", "*.html", "*.js"],
-}
+  ignorePatterns: ["*.scss", "*.html", "*.js"],
+};
